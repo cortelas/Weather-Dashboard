9cfc55e76cefecda6f9dda7ad544c1c38928942a
cortelas <cortelas1@yahoo.com>
2021-05-02 06:14:18 -0700

    stuck on lat

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,18 @@
 console.log("teststart");
 
-$("#inputButton").on("click", function(event){
-    console.log("the submit button was pressed");
-});
+function myFunction(){
+    console.log("the submit button was pressed myFunsction");
+    var searchTerm = document.querySelector("#inputCity").value;
 
+    fetch(
+        "https://geocode.xyz/" + searchTerm + "&auth=12178405404274779136x18400"
+    )
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(response){
+        console.log(response.data[0]);
+    });
+}
 
 console.log("testend");
